385d318283a7f9fb54d33e142fb42aa15cc72972
Victor Șevcenco <nicee.sevcenco@gmail.com>
2024-08-01 22:42:29 +0300

    Added model to entity representation and configured OnModelCreating

--- backend/allure.API/allure.Infrastructure/AllureDbContext.cs
+++ backend/allure.API/allure.Infrastructure/AllureDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using allure.Domain.Models;
+using allure.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace allure.Infrastructure;
@@ -7,18 +9,40 @@ public class AllureDbContext : DbContext
 {
     private readonly IConfiguration _configuration;
 
-    public AllureDbContext(IConfiguration configuration)
+    public AllureDbContext(DbContextOptions<AllureDbContext> options, IConfiguration configuration)
+        : base(options)
     {
         _configuration = configuration;
     }
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<CartProduct> CartProducts => Set<CartProduct>();
+    public DbSet<DebitCard> DebitCards => Set<DebitCard>();
+    public DbSet<Order> Orders => Set<Order>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Review> Reviews => Set<Review>();
+    public DbSet<ShippingAddress> ShippingAddresses => Set<ShippingAddress>();
+    public DbSet<Shop> Shops => Set<Shop>();
+    public DbSet<User> Users => Set<User>();
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("AllureDb"));
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("AllureDb"));
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new CartProductConfiguration());
+        modelBuilder.ApplyConfiguration(new DebitCardConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        modelBuilder.ApplyConfiguration(new ReviewConfiguration());
+        modelBuilder.ApplyConfiguration(new ShippingAddressConfiguration());
+        modelBuilder.ApplyConfiguration(new ShopConfiguration());
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
     }
 }
